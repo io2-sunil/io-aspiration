816fc0ecd1177483f8414eade820d5ddef142c12
Sunil <io2.sunil2@gmail.com>
2022-07-08 07:25:24 +0530

    img

---
+++ reactfile/src/componets/Navbar/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+const Navbar = () => {
+  return (
+    <>
+      <div className="nav">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>view</li>
+          <li>Help</li>
+        </ul>
+      </div>
+    </>
+  );
+};
+export default Navbar;
